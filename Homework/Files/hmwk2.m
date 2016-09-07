f88ab8998273f9d35c464ac6a168fea1d70be773
[ianblackstone] <ianblackstone@boisestate.edu>
2016-09-07 14:39:57 -0600

    problem 1 and 2

--- Homework/Files/hmwk2.m
+++ Homework/Files/hmwk2.m
@@ -1,7 +1,28 @@
-n = input('input the number of terms to sum')
+%% Problem 1
+n = input('input the number of terms to sum: ');
 
-X = zeros(1,n);
+X = 0;
+pie = zeros(1,n);
 
 for k = 0:n
-	X(k + 1) = (-1)^k / (2 * k + 1) * ((4 * (1 / 5)^(2 * k + 1) - (1/239)^(2 * k + 1)));
+	X = X + (-1)^k / (2 * k + 1) * ((4 * (1 / 5)^(2 * k + 1) - (1/239)^(2 * k + 1)));
+    pie(k+1) = X;
 end
+
+pie = 4*pie;
+
+pieerror = abs(pie/pi - 1)*100;
+
+semilogy(pieerror)
+
+%% Problem 2
+n = 50;
+
+X = zeros(1,n);
+X(end) = 1/n;
+
+for k = 1:n
+    X(n-k) = n-k+1 + 1/X(n-k+1);
+end
+
+%% Problem 3
