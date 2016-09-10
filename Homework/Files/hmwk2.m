9bb0233f0a796fceb26f74e711e9f63ad7567777
[ianblackstone] <ianblackstone@boisestate.edu>
2016-09-09 19:42:26 -0600

    problem 3

--- Homework/Files/hmwk2.m
+++ Homework/Files/hmwk2.m
@@ -26,3 +26,44 @@ for k = 1:n
 end
 
 %% Problem 3
+
+
+
+%% Problem 6
+
+A = rand(2,3);
+B = rand(3);
+C = rand(3,2);
+D = rand(3);
+
+try
+    a = 2*A + C;
+catch 
+    warning('a cannot be calculated')
+    a = 0;
+end
+b = C - 3*B;
+c = 3*B - 2*D;
+d = A*D;
+e = C*A;
+f = A*C;
+g = B*D;
+h = D*B;
+i = B*C;
+j = C*B;
+k = D*A*B;
+l = 2*D^2 + B;
+m = D*D;
+n = A^2;
+o = C.' * D;
+p = B*A.';
+q = -2*A.' + 5*C;
+r = B.' + A*D;
+s = 0.5 * (B + B.');
+t = 0.5 * (B-B.');
+u = C*C.';
+v = C.' * C;
+w = (C*C.')^(-1);
+x = (C.' *C)^(-1);
+y = B* (A*D).';
+z = A*D*B.';
