65fe980bc15c9715a142733f888347c1b9f9396d
[ianblackstone] <ianblackstone@boisestate.edu>
2016-09-12 21:14:39 -0600

    hmwk 2 update

--- Homework/Files/hmwk2.m
+++ Homework/Files/hmwk2.m
@@ -38,32 +38,37 @@ D = rand(3);
 
 try
     a = 2*A + C;
-catch 
-    warning('a cannot be calculated')
-    a = 0;
+    c = 3*B - 2*D;
+    d = A*D;
+    e = C*A;
+    f = A*C;
+    g = B*D;
+    h = D*B;
+    i = B*C;
+    j = C*B;
+    k = D*A*B;
+    l = 2*D^2 + B;
+    m = D*D;
+    n = A^2;
+    o = C.' * D;
+    p = B*A.';
+    q = -2*A.' + 5*C;
+    r = B.' + A*D;
+    s = 0.5 * (B + B.');
+    t = 0.5 * (B-B.');
+    u = C*C.';
+    v = C.' * C;
+    w = (C*C.')^(-1);
+    x = (C.' *C)^(-1);
+    y = B* (A*D).';
+    z = A*D*B.';
+catch error
+    a = NaN;
 end
-b = C - 3*B;
-c = 3*B - 2*D;
-d = A*D;
-e = C*A;
-f = A*C;
-g = B*D;
-h = D*B;
-i = B*C;
-j = C*B;
-k = D*A*B;
-l = 2*D^2 + B;
-m = D*D;
-n = A^2;
-o = C.' * D;
-p = B*A.';
-q = -2*A.' + 5*C;
-r = B.' + A*D;
-s = 0.5 * (B + B.');
-t = 0.5 * (B-B.');
-u = C*C.';
-v = C.' * C;
-w = (C*C.')^(-1);
-x = (C.' *C)^(-1);
-y = B* (A*D).';
-z = A*D*B.';
+
+try
+    b = C - 3*B;
+catch error
+    b = NaN;
+end
+
