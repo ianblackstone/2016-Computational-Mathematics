8fbb2c06e3ca3a70b1149cc7f91e155c58fd39d6
Ian Blackstone <ianblackstone@boisestate.edu>
2016-09-14 22:59:46 -0600

    Homework 2 problem 6
    Problem 6 solved

--- Homework/Files/hmwk2.m
+++ Homework/Files/hmwk2.m
@@ -1,5 +1,5 @@
 %% Problem 1
-n = input('input the number of terms to sum: ');
+n = inut('input the number of terms to sum: '),
 
 X = 0;
 pie = zeros(1,n);
@@ -31,44 +31,28 @@ end
 
 %% Problem 6
 
+problems = {@(A,B,C,D) 2*A + C, @(A,B,C,D) C - 3*B, @(A,B,C,D) 3*B - 2*D,@(A,B,C,D) A*D, @(A,B,C,D) C*A, @(A,B,C,D) A*C, @(A,B,C,D) B*D, @(A,B,C,D) D*B, @(A,B,C,D) B*C, @(A,B,C,D) C*B, @(A,B,C,D) D*A*B, @(A,B,C,D) 2*D.' + B, @(A,B,C,D) D*D, @(A,B,C,D) A*A, @(A,B,C,D) C.' * D, @(A,B,C,D) B*A.', @(A,B,C,D) -2*A.' + 5*C, @(A,B,C,D) B.' + A*D, @(A,B,C,D) 0.5 * (B + B.'), @(A,B,C,D) 0.5 * (B-B.'), @(A,B,C,D) C*C.',@(A,B,C,D) C.' * C, @(A,B,C,D) (C*C.')^(-1), @(A,B,C,D) (C.' *C)^(-1), @(A,B,C,D) B* (A*D).', @(A,B,C,D) A*D*B.'};
+
 A = rand(2,3);
 B = rand(3);
 C = rand(3,2);
 D = rand(3);
 
-try
-    a = 2*A + C;
-    c = 3*B - 2*D;
-    d = A*D;
-    e = C*A;
-    f = A*C;
-    g = B*D;
-    h = D*B;
-    i = B*C;
-    j = C*B;
-    k = D*A*B;
-    l = 2*D^2 + B;
-    m = D*D;
-    n = A^2;
-    o = C.' * D;
-    p = B*A.';
-    q = -2*A.' + 5*C;
-    r = B.' + A*D;
-    s = 0.5 * (B + B.');
-    t = 0.5 * (B-B.');
-    u = C*C.';
-    v = C.' * C;
-    w = (C*C.')^(-1);
-    x = (C.' *C)^(-1);
-    y = B* (A*D).';
-    z = A*D*B.';
-catch error
-    a = NaN;
-end
+[size,length] = size(problems);
 
-try
-    b = C - 3*B;
-catch error
-    b = NaN;
+for n = 1:length
+    try
+        problems{n}(A,B,C,D);
+        fprintf('%c \n',n+96)
+    catch MExc
+        if MExc.identifier == 'MATLAB:dimagree'
+            fprintf('%c cannot be performed, matrix dimensions do not agree \n',n+96)
+        elseif MExc.identifier == 'MATLAB:innerdim'
+            fprintf('%c cannot be performed, inner matrix dimensions do not agree \n',n+96)
+        elseif MExc.identifier == 'MATLAB:mpower:notScalarAndSquareMatrix'
+            fprintf('%c cannot be performed, inputs are not a scalar and a square matrix \n',n+96)
+        else
+            fprintf('An unexpected error ocurred when calculating %c',n+96)
+        end
+    end
 end
-
