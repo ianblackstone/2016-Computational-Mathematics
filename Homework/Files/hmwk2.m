ff3f9990761f2c1846346773c85c9a23b2b46e68
Ian Blackstone <ianblackstone@boisestate.edu>
2016-09-15 13:46:12 -0600

    Commenting
    Adding comments to problems 1, 2, and 6

--- Homework/Files/hmwk2.m
+++ Homework/Files/hmwk2.m
@@ -1,26 +1,36 @@
 %% Problem 1
-n = inut('input the number of terms to sum: '),
+% Ask for a user input
+n = input('input the number of terms to sum: '),
 
+% initialize a variable and a list of zeros
 X = 0;
 pie = zeros(1,n);
 
+% Sum each term up to n terms, storing each succesive approximation in the pie list
 for k = 0:n
 	X = X + (-1)^k / (2 * k + 1) * ((4 * (1 / 5)^(2 * k + 1) - (1/239)^(2 * k + 1)));
     pie(k+1) = X;
 end
 
+% Multiply every approximation by 4.
 pie = 4*pie;
 
+% Create a new array with the error between pi and our approximation
 pieerror = abs(pie/pi - 1)*100;
 
+% Plot the error on a logarithmic scale
 semilogy(pieerror)
 
+
 %% Problem 2
+% Declare the number of items to sum
 n = 50;
 
+% Create a list of zeros and set the last item in that list equal to 1/n
 X = zeros(1,n);
 X(end) = 1/n;
 
+% Starting at the first nonzero item in the list (n-k+1) generate the next item in the list
 for k = 1:n
     X(n-k) = n-k+1 + 1/X(n-k+1);
 end
@@ -29,21 +39,36 @@ end
 
 
 
+%% Problem 4
+
+
+
+%% Problem 5
+
+
+
 %% Problem 6
 
+% Creating a list to hold all operations to be done, all stored as anonymous functions with A,B,C, and D as the inputs.  All four variables must be used as inputs for each item so the for loop can pass the proper inputs to each list item
 problems = {@(A,B,C,D) 2*A + C, @(A,B,C,D) C - 3*B, @(A,B,C,D) 3*B - 2*D,@(A,B,C,D) A*D, @(A,B,C,D) C*A, @(A,B,C,D) A*C, @(A,B,C,D) B*D, @(A,B,C,D) D*B, @(A,B,C,D) B*C, @(A,B,C,D) C*B, @(A,B,C,D) D*A*B, @(A,B,C,D) 2*D.' + B, @(A,B,C,D) D*D, @(A,B,C,D) A*A, @(A,B,C,D) C.' * D, @(A,B,C,D) B*A.', @(A,B,C,D) -2*A.' + 5*C, @(A,B,C,D) B.' + A*D, @(A,B,C,D) 0.5 * (B + B.'), @(A,B,C,D) 0.5 * (B-B.'), @(A,B,C,D) C*C.',@(A,B,C,D) C.' * C, @(A,B,C,D) (C*C.')^(-1), @(A,B,C,D) (C.' *C)^(-1), @(A,B,C,D) B* (A*D).', @(A,B,C,D) A*D*B.'};
 
+% Generate 4 matrices using random numbers
 A = rand(2,3);
 B = rand(3);
 C = rand(3,2);
 D = rand(3);
 
+% Determine the length of the problem set to be done to iterate over the correct number
 [size,length] = size(problems);
 
+% Iterate over each item in the list.
 for n = 1:length
+    % Try the nth item in the problem set, catching any errors and outputting the reason for the error.  Problem w throws a warning that is not caught by this code, but it does not prevent the code from running.
+    % I could not find a method of catching warnings and errors at the same time and it appears the only way to silence the warning is a user setting.
+    % The second elseif term is to catch problem n if it is entered as A^2 rather than A*A.  A^2 gives an error because A is not a square matrix, while A*A warns about inner dimensions not matching.
     try
         problems{n}(A,B,C,D);
-        fprintf('%c \n',n+96)
+        fprintf('%c complete \n',n+96)
     catch MExc
         if MExc.identifier == 'MATLAB:dimagree'
             fprintf('%c cannot be performed, matrix dimensions do not agree \n',n+96)
