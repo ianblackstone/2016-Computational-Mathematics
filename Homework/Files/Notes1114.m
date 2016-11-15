80afc8f4cbece0250b77ba13f0b2b7a476c240f0
[ianblackstone] <ianblackstone@boisestate.edu>
2016-11-15 10:50:01 -0700

    Notes

---
+++ Homework/Files/Notes1114.m
@@ -0,0 +1,13 @@
+function f = LotkaVoltera(t,y)
+
+alpha = 0.01
+f = [2*y(1) -alpha]
+
+ 
+ 
+% 
+% y0 = [300;150];
+% tspan = [0 10];
+% opt = odeset('AbsTol',1e-8,'RelTol',1e-8)
+% [t,y] = ode45(@LotkaVoltera,tspan,y0)
+% 
