d2f6e89066cc34cd8f3ec46f575751f08189934e
Ian Blackstone <ianblackstone@boisestate.edu>
2016-11-21 00:28:03 -0700

    Homework 6 done

--- Homework/Files/HW6_FujishinBlackstone.m
+++ Homework/Files/HW6_FujishinBlackstone.m
@@ -23,12 +23,21 @@ end
 %% Problem #1 : Degree of Precision
 function prob1()
 % Part A
+
+% Set bounds of integration
 a=0;
 b=1;
-n = 100;
+
+% set maximum loops.
+n = 10;
+
+% Our function to integrate
 f = @(x,k) x^k;
+
+% The exact integral.
 exct = @(x,k) 1/(k+1)*x^(k-1);
 
+% Loop to find the highest value of k that gives an accurate answer.
 for k = 0:n
 T = (b-a)/2*(f(a,k) + f(b,k));
 err = abs(exct(b,k)-T);
@@ -40,7 +49,9 @@ end
 
 % Part B: Simpson's rule
 c =(a+b)/2;
-h =(b-a);
+h = (b-a);
+
+% Loop to find the highest value of k that gives an accurate answer.
 for k = 0:n
     M = h*f((a+b)/2,k);
     T = h*(f(a,k)+f(b,k))/2;
@@ -54,78 +65,97 @@ end
 
 end
 
-%% Problem #2 :
+%% Problem #2 : Gaussian Quadrature
 function prob2()
-% Our given/Known Values
+
+% Part a
+
+% Our given evaluation points and weights.
 xi = [-0.96816023950763 -0.83603110732664 -0.61337143270059  -0.32425342340381 0 0.32425342340381 0.61337143270059 0.83603110732664 0.96816023950763]';
 ci = [0.081274388361574 0.18064816069486 0.26061069640294 0.31234707704 0.33023935500126 0.31234707704 0.26061069640294 0.18064816069486 0.081274388361574]';
-n = 9; % Our iteration number
 
-% Part A f1
-f1= @(x) ((x-1).^2).*exp(-(x.^2))
-I1 = integral(f1,-1,1); % Exact integral of f1
-GQForm1=0; % Start our initial use of GQ formula
-for i =1:n
-GQForm1 = GQForm1+(ci(i)*f1(xi(i))); % Summation of the GQ formula
-abserror1(i) = abs(I1-GQForm1);
-end
-iteration = [1:n]';
-error = abserror1';
-UsingGQf1 = table(iteration,xi,ci,error)
+% Define our functions
+f1 = @(x) ((x-1).^2).*exp(-(x.^2));
+f2 = @(x) 2*(1./(1+x.^2));
 
-% Part A f2
-f2 = @(x) 2*(1./(1+x.^2))
-I2 = integral(f2,-1,1);
-GQForm2=0;
-for i =1:n
-GQForm2 = GQForm2+(ci(i)*f2(xi(i))); % Summation of the GQ formula
-abserror2(i) = abs(I2-GQForm2);
-end
-iteration = [1:n]';
-error = abserror2';
-UsingGQf2 = table(iteration,xi,ci,error)
+% Calculate the Gaussian quadrature of each function, the exact integral, and the absolute error.
+GQ1 = sum(ci.*f1(xi));
+exact1 = integral(f1,-1,1);
+error1 = abs(exact1-GQ1);
 
-% Part B
-% Let us set-up the Simpson's Rule:
+GQ2 = sum(ci.*f2(xi));
+exact2 = integral(f2,-1,1);
+error2 = abs(exact2-GQ2);
+
+% Report the errors.
+fprintf('The error for the Gaussian quadrature of function 1 is %.3e. \n',error1)
+fprintf('The error for the Gaussian quadrature of function 2 is %.3e. \n',error2)
+
+% Part b
+
+% For this poart we will call a function stored in an external file:
+% function x = simps(a,b,n,f)
+% % The function implements the composite Simpson's rule
+
+% h = (b-a)/n;
+% x = zeros(1,n+1);
+% x(1) = a;
+% x(n+1) = b;
+% p = 0;
+% q = 0;
+
+% % Define the x-vector
+% for i = 2:n
+%     x(i) = a + (i-1)*h;
+% end
+
+% % Define the terms to be multiplied by 4
+% for i = 2:((n+1)/2)
+%     p = p + (f(x(2*i -2)));
+% end
+
+% % Define the terms to be multiplied by 2
+% for i = 2:((n-1)/2)
+%     q = q + (f(x(2*i -1)));
+% end
+
+% % Calculate final output
+% x = (h/3)*(f(a) + 2*q + 4*p + f(b));
+
+% end
+
+% Let us set-up the Simpson's Rule.
 S1 = simps(-1,1,8,f1);
-errorS1=abs(I1-S1)
+errorS1 = abs(exact1-S1);
 S2 = simps(-1,1,8,f2);
-errorS2=abs(I2-S2)
-% To compare errors side by side:
-errors1 = [errorS1;abserror1(end)];
-errors2 = [errorS2;abserror2(end)];
-Array = {'Simpsons Rule','GQ Formula'};
-T=table(errors1,errors2,'RowNames',Array)
-% From this we can compare and see the GQFormula is much more accurate than
-% Simpsons
+errorS2 = abs(exact2-S2);
 
-% Part C
-% Determine degree of precision of 9-point GQ formula of f1
-a = -1;
-b = 1;
-n = 9;
-f1 = @(x,k) ((x-1).^2).*exp(-(x.^2)).^k
-for k = 0:n
-T = (b-a)/2*(f1(a,k) + f1(b,k));
-exct = @(x) integral(@(x) f1(x,k),a,b); % Since the function alone won't integrate, I have to make bypass function
-err = abs(exct(b)-T);
-if err >= 1E-14
-	fprintf('m for part f1 = % .i \n',k-1)
-	break
-end
-end
+% Report the errors.
+fprintf('The error for the Simpsons approximation of function 1 is %.3e. \n',errorS1)
+fprintf('The error for the Simpsons approximation of function 2 is %.3e. \n',errorS2)
 
-% Determine degree of precision of 9-point GQ formula of f2
-a = -1;
-b = 1;
-n = 9;
-f2 = @(x,k) (2*(1./(1+x.^2))).^k;
+% The simpsons rule is less accurate for estimating an integral than Gaussian quadrature is.
+
+% Part c
+
+a=-1;
+b=1;
+
+% set maximum loops.
+n = 20;
+
+% Our function to integrate
+f = @(x,k) x.^k;
+
+% The exact integral.
+exact = @(x,k) 1/(k+1).*x.^(k-1);
+
+% Loop to find the highest value of k that gives an accurate answer.
 for k = 0:n
-T = (b-a)/2*(f2(a,k) + f2(b,k));
-exct = @(x) integral(@(x) f2(x,k),a,b); % Since the function alone won't integrate, I have to make bypass function
-err = abs(exct(b)-T);
+GQ = sum(ci.*f(xi,k));
+err = abs(exact(b,k) - exact(a,k) - GQ);
 if err >= 1E-14
-	fprintf('m for part f2 = \n',k-1)
+	fprintf('The degree of precision for Gausssian quadtrature, m = %.i \n',k-1)
 	break
 end
 end
@@ -133,10 +163,11 @@ end
 end
     
     
-%% Problem #3 : 
+%% Problem #3 : Integral Equations
 function prob3()
 
 % Part a
+
 % Define parameters and conditions.
 N = 1000;
 h = 1/N;
@@ -167,7 +198,10 @@ for x = j
 end
 
 % Plot the data.
+figure()
 plot(h.*j,uj)
+xlabel('Position Along Rod (m)')
+ylabel('Temperature (K)')
 
 % Part b
 
@@ -175,26 +209,33 @@ plot(h.*j,uj)
 u2 = @(x) (a-quad(f1,0,x))*(1-x) + (b+quad(f2,x,1))*x - 343.15;
 
 % Use fzero and an initial guess of 0.25 to find a zero.
-burn = fzero(u2,0.25)
+burn = fzero(u2,0.25);
+fprintf('You will burn yourself starting at %.2f meters along the rod.\n',burn)
 
 end
 
 
-%% Problem #4 :
+%% Problem #4 : Quasi-Monte Carlo Integration
 function prob4()
 
 % Part a
+
 % The exact answer is -512/pi^6 as given by the following code:
-% syms a b c d e f
-% exact = int(int(int(int(int(int(sin(pi/2*(a + b + c + d + e + f)),a,0,1),b,0,1),c,0,1),d,0,1),e,0,1),f,0,1)
+syms a b c d e f
+exact = int(int(int(int(int(int(sin(pi/2*(a + b + c + d + e + f)),a,0,1),b,0,1),c,0,1),d,0,1),e,0,1),f,0,1);
 
 % Part b
+
+% Set the number of points and generate the random points based on a seed.
+tic;
 N = 10000;
 rng(11032016);
 MCpoints = rand(N,6);
 
-g = @(a,b,c,d,e,f) sin(pi/2*(a + b + c + d + e + f))
+% Define our function.
+g = @(a,b,c,d,e,f) sin(pi/2*(a + b + c + d + e + f));
 
+% Set the points for each variable.
 a = MCpoints(:,1);
 b = MCpoints(:,2);
 c = MCpoints(:,3);
@@ -202,11 +243,18 @@ d = MCpoints(:,4);
 e = MCpoints(:,5);
 f = MCpoints(:,6);
 
-MC = sum(g(a,b,c,d,e,f))/N
+% Calculate the integral using the random points.
+MC = sum(g(a,b,c,d,e,f))/N;
+MCtime = toc;
+fprintf('The Monte Carlo method gives us %.5f, while the exact answer is %.5f. The error is %.5f. This calculation took %.5f seconds. \n',MC,exact,exact-MC, MCtime)
 
 % Part c
+
+% Generate points from the Halton set.
+tic;
 HSpoints = net(haltonset(6),N);
 
+% Set each variable to our points from the Halton set
 a = HSpoints(:,1);
 b = HSpoints(:,2);
 c = HSpoints(:,3);
@@ -214,33 +262,45 @@ d = HSpoints(:,4);
 e = HSpoints(:,5);
 f = HSpoints(:,6);
 
-HS = sum(g(a,b,c,d,e,f))/N
+% Calculate the integral based on the Halton set.
+HS = sum(g(a,b,c,d,e,f))/N;
+HStime = toc;
+fprintf('The Halton set gives us %.5f, while the exact answer is %.5f. The error is %.5f. This calculation took %.5f seconds. \n',HS,exact,exact-HS,HStime)
 
 % Part d
 
+% Define number of points and dimensionality.
+tic;
 n = 10;
 h = 1/n;
 d = 6;
-% Trapezoidal rule weights in one dimension
+
+% Trapezoidal rule weights in one dimension.
 w1 = h*[0.5;ones(n-1,1);0.5];
 w = w1;
-% Generate the trapezoidal rule weights in 6 dimensions
+
+% Generate the trapezoidal rule weights in 6 dimensions.
 for j=1:d-1
-w = bsxfun(@times,w,reshape(w1,[ones(1,j) n+1]));
+	w = bsxfun(@times,w,reshape(w1,[ones(1,j) n+1]));
 end
+
 % Grid of points
 [x1,x2,x3,x4,x5,x6] = ndgrid(linspace(0,1,n+1));
-g = w.*f(x1,x2,x3,x4,x5,x6);
-Qtrap = sum(g(:));
+m = w.*g(x1,x2,x3,x4,x5,x6);
+Qtrap = sum(m(:));
+Ttime = toc;
+fprintf('The trapezoidal method gives us %.5f, while the exact answer is %.5f. The error is %.5f. This calculation took %.5f seconds. \n',Qtrap,exact,exact-Qtrap,Ttime)
 
 % Part e
 
-
+% The trapezoidal rule gives a reasonably accurate answer with several orders of magnitude fewer points however the code takes longer than the random set to run.
+% The Halton set takes more time to run than random points but is an order of magnitude more accurate.
 
 end
 
-%% Problem #5 : 
+%% Problem #5 : Pursuit Curves
 function prob5()
+
 % Determine duration and step size.
 Tmax = 20;
 h = 0.1;
@@ -267,6 +327,9 @@ for x = 0:2
 	% Plot the results.
 	figure()
 	plot(Rpoints(:,1),Rpoints(:,2),'r-+',Fpoints(:,1),Fpoints(:,2),'k-o')
+    xlabel('X')
+    ylabel('Y')
+    title('Pursuit Curve')
 end
 
 end
