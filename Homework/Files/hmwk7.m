df8a9ae0a98e7780a53ba29ab8341763f75af8b6
Ian Blackstone <ianblackstone@boisestate.edu>
2016-11-30 16:34:25 -0700

    Homework 7

---
+++ Homework/Files/hmwk7.m
@@ -0,0 +1,21 @@
+%% Problem 1
+
+
+
+%% Problem 2
+
+% Initial conditions.
+x0 = 0;
+y0 = 0;
+v0 = 50;
+m = 15
+
+pos = zeros(2,)
+
+for k = 1:17
+	r = zeros(1,4);
+	theta = k*pi/36;
+	vx0 = 
+
+
+%% Problem 3
